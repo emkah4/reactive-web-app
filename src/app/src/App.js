b2fc27cc104ce4dafee9f6dadab5a94d606473d4
emkah4 <sherys.emilis@gmail.com>
2022-03-15 17:39:13 +0200

    card

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import Card from "./components/UI/Button/Card";
+
 function App() {
-  return <p>Hello world</p>
+  return <React.Fragment>
+    <Card>
+      <p>Hello World!</p>
+    </Card>
+  </React.Fragment>
 }
 
 export default App;
