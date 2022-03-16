a64ba8f43b8e916260e313485bba56955038d753
Valma Julius <julius.valma@mif.stud.vu.lt>
2022-03-16 20:46:43 +0200

    added-footer

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 // Components
 import Header from "./components/Layout/Header";
+import Footer from "./components/Layout/Footer";
 
 function App() {
   return (
     <React.Fragment>
       <Header></Header>
       <p>Hello, Reactive</p>
+      <Footer></Footer>
     </React.Fragment>
   );
 }
--- src/app/src/components/Layout/Header.js
+++ src/app/src/components/Layout/Header.js
@@ -1,20 +1,37 @@
 import React from "react";
 
 // Components
-import Navigation from "./HeaderParts/Navigation";
-import NavigationLogo from "./HeaderParts/NavigationLogo";
+import Navigation from "./Navigation";
+import NavigationLogo from "./LayoutAssets/NavigationLogo";
 import Button from "../UI/Button/Button";
 
 // Styles
 import style from "./Header.module.css";
 
 const Header = (props) => {
+  const navList = [
+    {
+      id: "footer-nav-1",
+      title: "Create a script",
+      link: "#",
+    },
+    {
+      id: "footer-nav-2",
+      title: "My scripts",
+      link: "#",
+    },
+    {
+      id: "footer-nav-3",
+      title: "About",
+      link: "#",
+    },
+  ];
   return (
     <header className={style.header}>
       <NavigationLogo />
       <div className={style.container}>
         <div className={style.navigation}>
-          <Navigation />
+          <Navigation className={style.ul} navigationList={navList} />
         </div>
         <div>
           <Button className={style.button}>Login</Button>
