5e13695d444b6c0564be57abab50c93fd795b8bd
Bendaravičius Deividas <deividas.bendaravicius@mif.stud.vu.lt>
2022-03-17 13:31:40 +0200

    Revert "Merge branch 'RCTV-16-Header' into 'master'"
    This reverts merge request !4

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,17 +1,5 @@
-import React from "react";
-
-// Components
-import Header from "./components/Layout/Header";
-import Footer from "./components/Layout/Footer";
-
 function App() {
-  return (
-    <React.Fragment>
-      <Header></Header>
-      <p>Hello, Reactive</p>
-      <Footer></Footer>
-    </React.Fragment>
-  );
+  return <p>Hello world</p>
 }
 
 export default App;
