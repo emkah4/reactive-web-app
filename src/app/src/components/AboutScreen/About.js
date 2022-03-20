a4d8236cf3b51028d61edb5c348973002263dae3
emkah4 <sherys.emilis@gmail.com>
2022-03-20 15:00:56 +0200

    react-router-dom navigation added

---
+++ src/app/src/components/AboutScreen/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import styles from './About.module.css'
+
+const About = () => {
+    return <div className={styles.container}>
+        <h1>ABOUT</h1>
+        <p>Est adipisicing ad ut quis. Consectetur est esse labore id ullamco est elit ex adipisicing veniam adipisicing velit ullamco sint. Sit eu exercitation deserunt deserunt. Anim in sint cillum magna amet consectetur cillum fugiat enim sit culpa nisi. Duis sit laboris incididunt tempor culpa dolore aute in ea reprehenderit ullamco.</p>
+    </div>
+}
+
+export default About;
---
+++ src/app/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as MyScripts } from "./MyScriptsScreen/MyScripts";
+export { default as BuildScreen } from "./BuildScreen/BuildScreen";
+export { default as About } from "./AboutScreen/About";
+export { default as Home } from "./HomeScreen/Home";
