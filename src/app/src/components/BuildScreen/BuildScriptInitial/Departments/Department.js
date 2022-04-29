74ed9180ddafb82f5e24b58ee74a18874c7d77d5
debe7408 <deividas.bendaravicius@mif.stud.vu.lt>
2022-04-29 20:14:02 +0300

    Fixed few lines

--- src/app/src/components/BuildScreen/BuildScriptInitial/Departments/Department.js
+++ src/app/src/components/BuildScreen/BuildScriptInitial/Departments/Department.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-import styles from "./Department.module.css";
 import AddPeoplePopup from "./AddPeoplePopup/AddPeoplePopup";
 import ListGroup from "react-bootstrap/ListGroup";
 
 const Departments = (props) => {
   const listOfPeople = props.data.dept_people;
   const [showModal, setShowModal] = useState(false);
-  const [membersList, setMembersList] = useState(listOfPeople);
 
   const handleModalShow = (event) => {
     setShowModal(true);
@@ -22,7 +20,7 @@ const Departments = (props) => {
     <React.Fragment>
       <ListGroup.Item key={props.data.dept_id} action onClick={handleModalShow}>
         <h3>{props.data.dept_name}</h3>
-        {membersList.length === 0 && (
+        {listOfPeople.length === 0 && (
           <small>No members yet, click to add!</small>
         )}
       </ListGroup.Item>
